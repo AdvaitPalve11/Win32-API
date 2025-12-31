d6593a0a4d37c0404e21691cc8c5b4f4fcfd10a2
Advait Palve <advaitdpalve@gmail.com>
2025-12-31 13:12:53 +0530

    Add Clean exe ps1 File

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hIntstance , HINSTANCE hPrev, LPSTR args,int ncmd){
+
+  MessageBox(NULL,"Welcome in WIN API ","Message Box",MB_OKCANCEL);
+  return 0;
+}
