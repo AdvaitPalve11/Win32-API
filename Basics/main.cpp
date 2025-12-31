6a71aa559ae992d52e270d45f2a2dfc013e23bec
Advait Palve <advaitdpalve@gmail.com>
2025-12-31 13:55:57 +0530

    Create a Message Box

--- Basics/main.cpp
+++ Basics/main.cpp
@@ -1,8 +1,29 @@
 #include<windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam){
+  switch (uMsg)
+  {
+  case WM_CLOSE:
+    PostQuitMessage(0);
+    return 0;
+    break;
+    case WM_KEYDOWN:
+      if(wParam == 'A')
+        MessageBox(hWnd,"You Pressed A","Key",MB_OKCANCEL);
+  default:
+    break;
+  }
+
+   return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
 
 int WINAPI WinMain(HINSTANCE hIntstance , HINSTANCE hPrev, LPSTR args,int ncmd){
 
-  MessageBox(NULL,"Welcome in WIN API ","Message Box",MB_OKCANCEL);
-  return 0;
+  WNDCLASSW wc ={};
+  wc.lpfnWndProc = WndProc;
+  wc.hInstance = hIntstance;
+  wc.lpszClassName = L"Basic Window";
+
+  CreateWindow("Basic Window" , "First Window" , WS_OVERLAPPEDWINDOW,100,100,800,600,NULL,NULL,hIntstance,NULL);
 }
+
